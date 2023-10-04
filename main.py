c3693f6da4a261e75833f373999bc16730e26878
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-10-04 19:41:56 +0000

    aa

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 """
 ETL-Query script
 """
-
+import fire
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
-# Extract
-print("Extracting data...")
-extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+def main(query_string):
+    # Extract
+    print("Extracting data...")
+    extract()
 
-# Query
-print("Querying data...")
-query()
+    # Transform and load
+    print("Transforming data...")
+    load()
+
+    # Query
+    print("Querying data...")
+    query(query_string)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -7,7 +7,7 @@ import sqlite3
 import pandas as pd
 
 
-def load(the_query):
+def load():
     dataset = "data/life_expectancy.csv"
     df = pd.read_csv(dataset)
     database_path = "/workspaces/as1466_sqlite_lab/LifeDB.db"
@@ -15,7 +15,7 @@ def load(the_query):
    
     df.to_sql("table1", connect, if_exists="replace", index=False)
     cursor = connect.cursor()
-    cursor.execute(the_query)
+    #cursor.execute(the_query)
     connect.commit()
     connect.close()
   
