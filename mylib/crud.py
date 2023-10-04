4f64badc461e56de6a3bc17c7dbf58f799cd08ae
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-10-04 20:28:26 +0000

    adding crud operations script

---
+++ mylib/crud.py
@@ -0,0 +1,3 @@
+'''
+This file contains basic CRUD operations 
+'''
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -10,6 +10,11 @@ import pandas as pd
 def load():
     dataset = "data/life_expectancy.csv"
     df = pd.read_csv(dataset)
+    ## transforming database with Pandas 
+    column_mapping = {'Country Name': 'Country_name', 'Country Code': 'Country_code','Life Expectancy World Bank':'life_expectancy','Prevelance of Undernourishment':'undernourish',
+                  'Health Expenditure %':'health_exp'}
+    df = df.rename(columns=column_mapping)
+    df = df.iloc[:,:9]
     database_path = "/workspaces/as1466_sqlite_lab/LifeDB.db"
     connect = sqlite3.connect(database_path)
    
