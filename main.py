a654f4e745b57210bb09f2ec62ca85606f443d1c
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-10-04 14:32:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 """
 ETL-Query script
 """
-import fire
+
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
+# Extract
+print("Extracting data...")
+extract()
 
-def main(the_query):
-    """Run the ETL process"""
-    # Extract
-    print("Extracting data...")
-    extract()
+# Transform and load
+print("Transforming data...")
+load()
 
-    # Transform and load
-    print("Transforming data...")
-    load(the_query)
-
-    # Query
-    print("Querying data...")
-    query(the_query)
-
-
-if __name__ == "__main__":
-    
-    #load("SELECT COUNT(*) FROM table1")
-    fire.Fire(main)
-    #query("SELECT COUNT(*) FROM table1")
+# Query
+print("Querying data...")
+query()
