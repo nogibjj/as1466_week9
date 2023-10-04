2fd82d4b6bef8ad0facca0d13771878a04557743
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-10-04 20:44:25 +0000

    adding crud operations

--- mylib/crud.py
+++ mylib/crud.py
@@ -1,3 +1,47 @@
 '''
 This file contains basic CRUD operations 
 '''
+""""
+ETL-Query script
+"""
+
+import sqlite3
+
+
+# Your CRUD operation functions
+def create(query,  db_name='LifeDB.db'):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+def read(query, db_name='LifeDB.db'):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    user = cursor.fetchone()
+    connection.close()
+    return user
+
+def update(query, db_name='LifeDB.db'):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+def delete(query, db_name='lifeDB.db'):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
+def insert(query,  db_name='LifeDB.db'):
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+
